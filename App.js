da48e10c38abf588b02cc8b5b96b40722a2ed20e
NATALLY VITÓRIA OLIVEIRA DE ANDRADE <natally.andrade@etec.sp.gov.br>
2026-05-21 12:40:33 -0300

    aaaaaaa

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.txt}>ALCOOL OU GASOLINA</Text>
+      <Text style={styles.subtxt}>Preço do Etanol (R$):</Text>
+      <TextInput
+      style={styles.input}
+      placeholder='Gasolina'
+      placeholderTextColor={"#ffff"}
+      />
+
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +21,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#310d85',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  txt: {
+    color:'#ffff',
+    fontSize:20,
+    fontVariant:'negrito',
+  },
+  subtxt: {
+    color:'#ffff',
+    fontSize:15,
+    fontVariant:'negrito',
+    textAlign:'left',
+  },
+   input: {
+    fontSize:15,
+    alignItems:'center',
+    backgroundColor:'#ffffff74',
+    margin:5,
+    height:25,
+    borderColor:'#ffff',
+    padding:10,
+    borderRadius:5,
+   },
 });
